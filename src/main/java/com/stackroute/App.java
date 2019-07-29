3f75c8d55e384fbf8ab0c9b0a8d9c2522ab3cd9e
nishi.singh <nishi.singh@cgi.com>
2019-07-29 12:12:12 +0530

    task-5 completed

--- src/main/java/com/stackroute/App.java
+++ src/main/java/com/stackroute/App.java
@@ -23,18 +23,18 @@ public class App
 
     {
 
-        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        AbstractApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
         //task-1
-         Movie movie=(Movie)context.getBean("movie");
-      System.out.println(movie.getActor());
+        // Movie movie=(Movie)context.getBean("movie");
+      //System.out.println(movie.getActor());
         //task-2
-       Movie movieA=(Movie)context.getBean("movie1");
-       System.out.println(movieA.getActor());
-      System.out.println(movie=movieA);
+       //Movie movieA=(Movie)context.getBean("movie1");
+       //System.out.println(movieA.getActor());
+      //System.out.println(movie=movieA);
       //  BeanLifecycleDemoBean beanLifecycleDemoBean = context.getBean("BeanLifecycle", BeanLifecycleDemoBean.class);
         //System.out.println(beanLifecycleDemoBean);
 
-         //context.registerShutdownHook();
+         context.registerShutdownHook();
 
 
 
